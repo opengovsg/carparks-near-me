c7fa12ab3f9bae489c8683800c6d2f1b400df6c0
Nikhil <nikhil@data.gov.sg>
2019-10-02 17:39:17 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,126 @@
+const axios = require("axios");
+const parse = require("csv-parse/lib/sync");
+const fs = require("fs");
+
+// file reading and parsing done synchronously to prevent race condition
+var hdbCarparkInfoCSV = fs.readFileSync("hdb-carpark-information.csv", "utf8");
+const carparkStaticInfo = parse(hdbCarparkInfoCSV, {
+  columns: true,
+  skip_empty_lines: true
+});
+
+exports.handler = async function(event, context) {
+    console.log("Updating carpark availability on server");
+    const carparkAvailability = await axios
+      .get("https://api.data.gov.sg/v1/transport/carpark-availability", {})
+      .then(response => {
+        const carparkAvailability = response.data.items[0].carpark_data;
+        return carparkAvailability;
+      });
+    const carparks = combineCarparkData(carparkAvailability, carparkStaticInfo);
+
+    if (event.queryStringParameters) {
+      event = event.queryStringParameters;
+    }
+        console.log(
+          `Request with following parameters ${JSON.stringify(
+            event,
+            null,
+            2
+          )}`
+        );
+function combineCarparkData(carparkAvailability, carparks) {
+  function findMatchingCarpark(carparkNumber, carparks) {
+    matchedCarpark = null;
+    for (const carpark of carparks) {
+      if (carpark.car_park_no === carparkNumber) {
+        matchedCarpark = carpark;
+        break;
+      }
+    }
+    return matchedCarpark;
+  }
+
+  for (const carpark of carparkAvailability) {
+    const carparkNumber = carpark.carpark_number;
+    matched_carpark = findMatchingCarpark(carparkNumber, carparks);
+    if (matched_carpark !== null) {
+      matched_carpark.total_lots = carpark.carpark_info[0].total_lots;
+      matched_carpark.lots_available = carpark.carpark_info[0].lots_available;
+    }
+  }
+  return carparks;
+}
+
+async function getCarparkList(x, y) {
+    function addDistanceToCarparks(x, y) {
+    function distanceFromXY(carpark, x, y) {
+      const x2 = Math.pow(parseFloat(carpark.x_coord) - x, 2);
+      const y2 = Math.pow(parseFloat(carpark.y_coord) - y, 2);
+      return Math.sqrt(x2 + y2);
+    }
+
+    const carparksWithDistance = [];
+
+    // Calculate distance from every carpark to the location
+    for (const carpark of carparks) {
+      if (carpark.lots_available !== undefined) {
+        const distance = distanceFromXY(carpark, x, y);
+        carpark.distance = distance;
+        carparksWithDistance.push(carpark);
+      }
+    }
+
+    return carparksWithDistance;
+  }
+
+  function sortCarparksByDistance(carparks) {
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    // See the compareNumbers function
+    function compareCarparksByDistance(carpark1, carpark2) {
+      return carpark1.distance - carpark2.distance;
+    }
+
+    carparks.sort(compareCarparksByDistance);
+    return carparks;
+  }
+
+  // Calculate distance of every carpark to x and y
+    const carparksWithDistance = addDistanceToCarparks(x, y);
+
+  // Get the nearest n carparks
+  const sortedCarparks = sortCarparksByDistance(carparksWithDistance);
+  return sortedCarparks;
+}
+
+  const responseCarparks = await axios
+    .get("https://developers.onemap.sg/commonapi/search", {
+      params: {
+        getAddrDetails: "N",
+        returnGeom: "Y",
+        searchVal: event.location
+      }
+    })
+    .then(response => {
+      return response.data.results[0];
+    })
+    .then(location => {
+      return getCarparkList(location.X, location.Y);
+});
+
+  // format specified on https://aws.amazon.com/premiumsupport/knowledge-center/malformed-502-api-gateway/
+  const APIGatewayResponse = {
+    statusCode: 200,
+    // https://docs.aws.amazon.com/apigateway/latest/developerguide/how-to-cors.html
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify(
+      event.limit && event.limit > 1
+        ? responseCarparks.slice(0, event.limit)
+        : responseCarparks[0]
+    ),
+    isBase64Encoded: false
+  };
+  return APIGatewayResponse;
+};
---
+++ todo.md
@@ -0,0 +1,55 @@
+# TODOs
+
+PRs welcome!
+- explicitly state why things may be easy to do versus hard. Explicitly say that. Don’t be sheepish about it
+- show them the backend just for fun, the index.js file which is super complicated. They need to be humbled in some part.
+- why do we use ” and when do we not?
+- input breaking down
+- please extract zip files
+- what’s the difference? Is there a diff? We learned it in one chunk
+- figuring things out on the computer requires perseverance. It’s hard. People may not be willing to put in the effort
+- can we get to the point where they can do small task?
+- change the course descriptIno
+- change the money required to pay for the course
+
+
+# Feedback collated from instructors
+
+# Reflections and lessons learned
+
+- go do MOOCs to figure out how people are being taught this material
+- Don't do stuff that "will make sense later"
+- explain why the braces are used more coherently (group code together, encapsulate, ...)
+- what gets put into quotes and what doesn't? variables are not easy to understand
+- camel casing is hard to get right, especially that getElementById 
+- don't make text disappear and reappear. what is the point, no one gets
+- explain the ordering of inputs into the function and how that matters
+- when explaining functions, motivate why we need a "recipe" in the first place, that doesn't make sense
+- Less changing of screens from slides to code to browser - trainers know what to look at but participants don't, and hence they cannot keep up.
+- make sure that the content is easily copy-pasted over
+- explain more high level concepts, they don't need to write code, they just need to be able to speak to vendors more confidently
+- bigger printed out notes so that people don't confuse ) and } and ]?
+  
+Latest feedback from conversation with Doris, Director Executive Programs, CSC:
+- State explicitly the myths you want to debunk
+- It's helpful for people to type in code and see how the autocomplete helps you in programming
+- showing cause and effect with the code that they write
+- They should learn about the bigger picture of how coding is done. What software are you using? Did you have a choice? If you did, why did you choose what you did?
+- Try out the LEARN app
+- GovTech is working on something with David Eaves for directors and above to participate in 
+- Record the sharing over video and send it out to folks
+- explain why your team is valuable through the course
+
+## Feedback that has been addressed
+
+- Too many functions and functions calling other functions? You can tell by their questions that they don't know what's happening and just want their program to work.
+- Write more comments throughout the code explaining what's happening so that they can reference them whenever they are confused
+- More comments above chunks of code to explain what it's doing. So if participants are lost, at least they can read comments.
+- APIs are hard. Reduce it to only one API 
+- More skeleton - Have a good basic template, participants can just modify small chunks of code on their own, instead of just copying and pasting. e.g. <h1>YOUR HEADER HERE</h1> and they edit on their own
+- Specify that keydown is not ArrowDown
+- console is hard to understand, so try to avoid it and instead display text on screen using the div tags or alerts
+- put the javascript and css into the same page as the html code so that they don't need to toggle between files
+- change stuff to make sure that they don't appear, then tell them to make it disappear, then reappear it
+- Write comments in your code so that they know what is happening after you stop talking 
+- every milestone that the user does should be a change so that they know what the impact of writing the code is. Cause and effect should be made clear and explicit
